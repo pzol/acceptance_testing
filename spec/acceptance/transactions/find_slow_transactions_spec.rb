b7e8a78e45a9a192949abdf5010dc12a3fddf915
Piotr Zolnierek <pz@anixe.pl>
2012-01-29 20:30:03 +0100

    Rename method for more consistence

--- spec/acceptance/transactions/find_slow_transactions_spec.rb
+++ spec/acceptance/transactions/find_slow_transactions_spec.rb
@@ -11,14 +11,12 @@ story 'Developers can find slow transactions' do
     user.sees_transactions_table!
     
     # _ 'When I search for time_taken
-    user.searches_for_time_taken(time_taken = 6000)
+    user.searches_by_time_taken(time_taken = 6000)
 
     # _ 'Then I see only transactions which have time_taken longer than specified
     user.sees_transactions_table!
 
-    user.all_table_rows do |tr|
-      tr.time_taken.should > time_taken
-    end
+    user.all_table_rows { |tr| tr.time_taken.should > time_taken }
   end
 
   scenario 'sees all messages when entering an invalid value for time_taken' do
@@ -28,7 +26,7 @@ story 'Developers can find slow transactions' do
     user.visit_latest_transactions
 
     # _ 'When I search for time_taken with an invalid value 'xyz'
-    user.searches_for_time_taken(time_taken = 'xyz')
+    user.searches_by_time_taken(time_taken = 'xyz')
     user.sees_transactions_table!
     user.table_rows.should have(20).rows
   end
--- spec/acceptance/transactions/role_time_taken_searcher.rb
+++ spec/acceptance/transactions/role_time_taken_searcher.rb
@@ -1,6 +1,6 @@
 module Role
   module TimeTakenSearcher
-    def searches_for_time_taken(time_taken)
+    def searches_by_time_taken(time_taken)
       fill_in 'time_taken', :with => time_taken
       click_button 'searchButton'
     end
