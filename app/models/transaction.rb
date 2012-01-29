364b02e93f9c4bbe115d3d630fa883e393013aa4
Piotr Zolnierek <pz@anixe.pl>
2012-01-29 18:12:42 +0100

    Allow initialization of Transaction with hash

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,8 +4,16 @@ class Transaction
   attr_accessor :contract, :facility, :method, :product, :timestamp, :time_taken
   validates :time_taken, :numericality => { :only_integer => true }
 
+  def initialize(hash={})
+    hash.each do |key, value| 
+      method = "#{key}=".to_sym
+      send(method, value) if respond_to? method
+    end
+  end
+
   def attributes
     { contract: contract, facility: facility, method: method, 
       product: product, timestamp: timestamp, time_taken: time_taken}
   end
+  
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -8,4 +8,18 @@ describe Transaction do
     t.should_not be_valid
   end
 
+  it 'should initialize with a hash' do
+    hash = { contract: 'contract', facility: 'facility', method: 'method', 
+      product: 'product', timestamp: Time.at(13000000), time_taken: 100}
+      transaction = Transaction.new(hash)
+
+      transaction.contract.should == 'contract'
+      transaction.facility.should == 'facility'
+      transaction.method.should == 'method'
+      transaction.product.should == 'product'
+      transaction.timestamp.should == Time.at(13000000)
+      transaction.time_taken.should == 100
+      transaction.should be_valid
+  end
+
 end
