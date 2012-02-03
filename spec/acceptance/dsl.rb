360dd486c73e5b04ab0890908910bac3fb69d393
Piotr Zolnierek <pz@anixe.pl>
2012-02-03 05:57:30 +0100

    Add doc (_) method for extending the spec inside scenario

--- spec/acceptance/dsl.rb
+++ spec/acceptance/dsl.rb
@@ -1,12 +1,24 @@
-module AccepteanceTests
+module AcceptanceTests
   def self.included(base)
     base.instance_eval do
       alias :background :before
       alias :scenario :it
     end
+
+    base.class_eval do
+      attr_accessor :user
+
+      def _(text)
+        example.description << "\n    #{text}".white
+      end
+    end
+
   end
 end
-RSpec.configuration.include AccepteanceTests
+
+RSpec.configure do |config|
+  config.include AcceptanceTests
+end
 
 Kernel.class_eval do
   alias :story :describe
